6f9ef432b19860f063c4e20a30454338af53801d
Maybe <ava241109@gmail.com>
2026-04-02 15:07:29 +0500

    comment

---
+++ boshlash.js
@@ -0,0 +1,11 @@
+let start = document.getElementById('start')
+let later = document.getElementById('later')
+
+function Start() {
+    window.location.href = "page4.html";
+    window.location.href = "bugungiDars.html"
+}
+
+function Later() {
+    window.location.href = "page4.html";
+}
---
+++ signin.js
@@ -0,0 +1,52 @@
+const url = 'https://clefdjiuabnztkmdwnjc.supabase.co'
+const token = 'sb_publishable_IOtRCnm8B19gdABXAPKMqA__1-q_1BN'
+
+const _supabase = supabase.createClient(url, token)
+
+async function yubor() {
+    const ism = document.getElementById("ism").value.trim()
+    const password = document.getElementById("password").value.trim()
+    const email = document.getElementById("email").value.trim()
+    const password_accept = document.getElementById("password-accept").value.trim()
+
+    if (!ism || !email || !password || !password_accept) {
+        alert("Barcha maydonlarni to'ldiring")
+        return
+    }
+
+    if (password !== password_accept) {
+        alert("Parollar mos emas")
+        return
+    }
+
+    try {
+        const { data, error } = await _supabase
+            .from('signin')
+            .insert([
+                {
+                    ism: ism,
+                    email: email,
+                    parol: password
+                }
+            ])
+
+        if (error) {
+            throw error
+        }
+
+        alert("Siz ro'yhatdan o'tdingiz ✅")
+        window.location.href = "index.html"
+        tozala()
+
+    } catch (err) {
+        alert("Xatolik: " + err.message)
+        console.error(err)
+    }
+}
+
+function tozala() {
+    document.getElementById("ism").value = ''
+    document.getElementById("password").value = ''
+    document.getElementById("email").value = ''
+    document.getElementById("password-accept").value = ''
+}
